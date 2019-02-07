2c2f8e2a69d3c5b9d4d9170e6540e28656f0d828
Hans van Someren <j.vansomeren-1@tudelft.nl>
2019-02-07 14:48:31 +0100

    Generating commuting gates sets
    Also updated scheduler.h wrt deps at SINK: not all 0 degree but just close with lastreaders/writers/Ds
    Also updated scheduler.h, cz deps, twice adding to lastreaders/...
    Also updated scheduler.h, dep comment nicely prints depname
    Also updated scheduler.h, getgateparameters suspicious when cc_light_instr/cc_light_instr_type not there
    Added the latter to test_ideal.json

--- ql/arch/ideal/ideal_eqasm_compiler.h
+++ ql/arch/ideal/ideal_eqasm_compiler.h
@@ -1,9 +1,8 @@
 /**
  * @file   ideal_eqasm_compiler.h
- * @date   08/2017
- * @author Imran Ashraf
- *         Nader Khammassi
- * @brief  cclighteqasm compiler implementation
+ * @date   02/2019
+ * @author Hans van Someren
+ * @brief  ideal compiler implementation
  */
 
 #ifndef QL_IDEAL_EQASM_COMPILER_H
@@ -23,51 +22,93 @@ namespace ql
 namespace arch
 {
 
-ql::ir::bundles_t ideal_schedule_rc(ql::circuit & ckt,
-    const ql::quantum_platform & platform, size_t nqubits, size_t ncreg = 0)
+class Depgraph : public Scheduler
 {
-    IOUT("Resource constraint scheduling of ideal instructions ...");
-    scheduling_direction_t  direction;
-    std::string schedopt = ql::options::get("scheduler");
-    if ("ASAP" == schedopt)
-    {
-        direction = forward_scheduling;
-    }
-    else if ("ALAP" == schedopt)
-    {
-        direction = backward_scheduling;
-    }
-    else
-    {
-        EOUT("Unknown scheduler");
-        throw ql::exception("Unknown scheduler!", false);
+public:
 
-    }
-    resource_manager_t rm(platform, direction);
-
-    Scheduler sched;
-    sched.Init(ckt, platform, nqubits, ncreg);
-    ql::ir::bundles_t bundles;
-    if ("ASAP" == schedopt)
+    void show_deps()
     {
-        bundles = sched.schedule_asap(rm, platform);
+        InDegMap<ListDigraph> inDeg(graph);
+        for (ListDigraph::NodeIt n(graph); n != INVALID; ++n)
+        {
+            DOUT("Incoming unfiltered dependences of node " << ": " << instruction[n]->qasm() << " :");
+            std::list<ListDigraph::Arc> Rarclist;
+            std::list<ListDigraph::Arc> Darclist;
+            for( ListDigraph::InArcIt arc(graph,n); arc != INVALID; ++arc )
+            {
+                ListDigraph::Node srcNode  = graph.source(arc);
+                if (depType[arc] == WAW
+                ||  depType[arc] == RAW
+                ||  depType[arc] == DAW
+                ) {
+                    continue;
+                }
+                DOUT("... Encountering relevant " << DepTypesNames[depType[arc]] << " by q" << cause[arc] << " from " << instruction[srcNode]->qasm());
+                if (depType[arc] == WAR
+                ||  depType[arc] == DAR
+                ) {
+                    Rarclist.push_back(arc);
+                }
+                else
+                if (depType[arc] == WAD
+                ||  depType[arc] == RAD
+                ) {
+                    Darclist.push_back(arc);
+                }
+            }
+            while (Rarclist.size() > 1)
+            {
+                std::list<ListDigraph::Arc> TMParclist = Rarclist;
+                int  operand = cause[Rarclist.front()];
+                TMParclist.remove_if([this,operand](ListDigraph::Arc a) { return cause[a] != operand; });
+                if (TMParclist.size() > 1)
+                {
+                    DOUT("At " << instruction[n]->qasm() << " found commuting gates on q" << operand << ":");
+                    for ( auto a : TMParclist )
+                    {
+                        ListDigraph::Node srcNode  = graph.source(a);
+                        DOUT("... " << instruction[srcNode]->qasm() << " as " << DepTypesNames[depType[a]] << " by q" << cause[a]);
+                    }
+                }
+                Rarclist.remove_if([this,operand](ListDigraph::Arc a) { return cause[a] == operand; });
+            }
+            while (Darclist.size() > 1)
+            {
+                std::list<ListDigraph::Arc> TMParclist = Darclist;
+                int  operand = cause[Darclist.front()];
+                TMParclist.remove_if([this,operand](ListDigraph::Arc a) { return cause[a] != operand; });
+                if (TMParclist.size() > 1)
+                {
+                    DOUT("At " << instruction[n]->qasm() << " found commuting gates on q" << operand << ":");
+                    for ( auto a : TMParclist )
+                    {
+                        ListDigraph::Node srcNode  = graph.source(a);
+                        DOUT("... " << instruction[srcNode]->qasm() << " as " << DepTypesNames[depType[a]] << " by q" << cause[a]);
+                    }
+                }
+                Darclist.remove_if([this,operand](ListDigraph::Arc a) { return cause[a] == operand; });
+            }
+        }
     }
-    else if ("ALAP" == schedopt)
+
+    void do_schedule(resource_manager_t& rm, const ql::quantum_platform& platform)
     {
-        bundles = sched.schedule_alap(rm, platform);
+	    std::string schedopt = ql::options::get("scheduler");
+	    if ("ASAP" == schedopt)
+	    {
+	        schedule_asap(rm, platform);
+	    }
+	    else if ("ALAP" == schedopt)
+	    {
+	        schedule_alap(rm, platform);
+	    }
+	    else
+	    {
+	        EOUT("Unknown scheduler");
+	        throw ql::exception("Unknown scheduler!", false);
+	    }
     }
-    else
-    {
-        EOUT("Unknown scheduler");
-        throw ql::exception("Unknown scheduler!", false);
-
-    }
-
-    DOUT("have yet to print bundles");
-
-    IOUT("Resource constraint scheduling of ideal instructions [Done].");
-    return bundles;
-}
+};
 
 /**
  * ideal eqasm compiler
@@ -77,44 +118,101 @@ class ideal_eqasm_compiler : public eqasm_compiler
 public:
 
     size_t          num_qubits;
-    size_t          ns_per_cycle;
-    size_t          total_exec_time = 0;
+    size_t          cycle_time;
 
-#define __ns_to_cycle(t) ((size_t)t/(size_t)ns_per_cycle)
+private:
 
-public:
-
-
-    /*
-     * program-level compilation of qasm to ideal_eqasm
-     */
-    void compile(std::string prog_name, ql::circuit& ckt, ql::quantum_platform& platform)
+    void print(quantum_kernel& kernel, size_t perm)
     {
-        EOUT("deprecated compile interface");
-        throw ql::exception("deprecated compile interface", false);
+	    std::stringstream ss_output_file;
+	    ss_output_file << ql::options::get("output_dir") << "/" << kernel.name << "_" << perm << ".qasm";
+	    DOUT("writing permutation to '" << ss_output_file.str() << "' ...");
+	    std::stringstream ss_qasm;
+	    ss_qasm << "." << kernel.name << "_" << perm << '\n';
+	    ql::circuit& ckt = kernel.c;
+	    for (auto gp : ckt)
+	    {
+	        ss_qasm << '\t' << gp->qasm();
+	        // ss_qasm << "\t# " << gp->cycle
+	        ss_qasm << '\n';
+	    }
+	    size_t  depth = ckt.back()->cycle + std::ceil( static_cast<float>(ckt.back()->duration) / cycle_time) - ckt.front()->cycle;
+	    ss_qasm <<  "# depth=" << depth;
+	    ql::utils::write_file(ss_output_file.str(), ss_qasm.str());
+	}
+
+    scheduling_direction_t  get_direction()
+    {
+	    scheduling_direction_t  direction;
+	    std::string schedopt = ql::options::get("scheduler");
+	    if ("ASAP" == schedopt)
+	    {
+	        direction = forward_scheduling;
+	    }
+	    else if ("ALAP" == schedopt)
+	    {
+	        direction = backward_scheduling;
+	    }
+	    else
+	    {
+	        EOUT("Unknown scheduler");
+	        throw ql::exception("Unknown scheduler!", false);
+	
+	    }
+        return direction;
     }
 
+	void generate_permutations(quantum_kernel& kernel,
+	    const ql::quantum_platform & platform, size_t nqubits, size_t ncreg = 0)
+	{
+	    DOUT("Generate commutable variations of kernel circuit ...");
+	    ql::circuit& ckt = kernel.c;
+	    if (ckt.empty())
+	    {
+	        DOUT("Empty kernel " << kernel.name);
+	        return;
+	    }
+
+	    resource_manager_t rm(platform, get_direction());
+	    Depgraph sched;
+	    sched.Init(ckt, platform, nqubits, ncreg);
+	
+	    // have a dependence graph
+        sched.show_deps();
+
+	    // find the commutable sets of nodes
+	    // convert each to permutations and combine those
+	    int perm = 0;
+	    // for each permutation:
+	    //  - generate additional dependences
+	    //  - check dag, if not, continue
+	    //  - schedule
+	    sched.do_schedule(rm, platform);
+	    //  - print
+	    print(kernel, perm);
+	    //  - remove additional dependences
+        //  ...
+	    
+	    DOUT("Generate commutable variations of kernel circuit [Done]");
+    }
 
     // kernel level compilation
     void compile(std::string prog_name, std::vector<quantum_kernel> kernels, const ql::quantum_platform& platform)
     {
-        DOUT("Compiling " << kernels.size() << " kernels to generate CCLight eQASM ... ");
+        DOUT("Compiling " << kernels.size() << " kernels to generate commuting permutations ... ");
 
         load_hw_settings(platform);
 
         for(auto &kernel : kernels)
         {
             IOUT("Compiling kernel: " << kernel.name);
-            ql::circuit& ckt = kernel.c;
             auto num_creg = kernel.creg_count;
-            if (! ckt.empty())
-            {
-                // schedule with platform resource constraints
-                ql::ir::bundles_t bundles = ideal_schedule_rc(ckt, platform, num_qubits, num_creg);
-            }
+            generate_permutations(kernel, platform, num_qubits, num_creg);
         }
 
         DOUT("Compiling Ideal eQASM [Done]");
+        DOUT("============================");
+        DOUT("");
     }
 
 
@@ -130,7 +228,7 @@ private:
         try
         {
             num_qubits                                      = platform.hardware_settings[params[p++]];
-            ns_per_cycle                                    = platform.hardware_settings[params[p++]];
+            cycle_time                                    = platform.hardware_settings[params[p++]];
         }
         catch (json::exception e)
         {
@@ -138,15 +236,16 @@ private:
         }
     }
 
-    /**
-     * emit qasm code
+public:
+    /*
+     * program-level compilation of qasm to ideal_eqasm
      */
-    void emit_eqasm(bool verbose=false)
+    void compile(std::string prog_name, ql::circuit& ckt, ql::quantum_platform& platform)
     {
-        IOUT("emitting eqasm...");
-        eqasm_code.clear();
-        IOUT("emitting eqasm code done.");
+        EOUT("deprecated compile interface");
+        throw ql::exception("deprecated compile interface", false);
     }
+
 
 };
 }
--- tests/test_ideal.cc
+++ tests/test_ideal.cc
@@ -10,13 +10,139 @@
 
 #include <ql/openql.h>
 
-// steane qec on s7 with cnots
+// test cnot control operand commutativity
+// i.e. best result is the reverse original order
 void
-test_steaneqec(std::string v, std::string schedopt, std::string sched_post179opt)
+test_cnot_controlcommute(std::string v)
 {
     int n = 7;
-    std::string prog_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    std::string kernel_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
+    float sweep_points[] = { 1 };
+
+    ql::quantum_platform starmon("starmon", "test_ideal.json");
+    ql::set_platform(starmon);
+    ql::quantum_program prog(prog_name, starmon, n, 0);
+    ql::quantum_kernel k(kernel_name, starmon, n, 0);
+    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    k.gate("cnot", 3,0);
+    k.gate("cnot", 3,6);
+    k.gate("t", 6);
+    k.gate("y", 6);
+    k.gate("cnot", 3,1);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("cnot", 3,5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    prog.add(k);
+
+    prog.compile( );
+}
+
+// test cnot target operand commutativity
+// i.e. best result is the reverse original order
+void
+test_cnot_targetcommute(std::string v)
+{
+    int n = 7;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
+    float sweep_points[] = { 1 };
+
+    ql::quantum_platform starmon("starmon", "test_ideal.json");
+    ql::set_platform(starmon);
+    ql::quantum_program prog(prog_name, starmon, n, 0);
+    ql::quantum_kernel k(kernel_name, starmon, n, 0);
+    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    k.gate("cnot", 0,3);
+    k.gate("cnot", 6,3);
+    k.gate("t", 6);
+    k.gate("y", 6);
+    k.gate("cnot", 1,3);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("cnot", 5,3);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    prog.add(k);
+
+    prog.compile( );
+}
+
+// test cz any operand commutativity
+// i.e. best result is the reverse original order
+void
+test_cz_anycommute(std::string v)
+{
+    int n = 7;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
+    float sweep_points[] = { 1 };
+
+    ql::quantum_platform starmon("starmon", "test_ideal.json");
+    ql::set_platform(starmon);
+    ql::quantum_program prog(prog_name, starmon, n, 0);
+    ql::quantum_kernel k(kernel_name, starmon, n, 0);
+    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    k.gate("cz", 0,3);
+    k.gate("cz", 3,6);
+    k.gate("t", 6);
+    k.gate("y", 6);
+    k.gate("cz", 1,3);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("t", 1);
+    k.gate("y", 1);
+    k.gate("cz", 3,5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+    k.gate("t", 5);
+    k.gate("y", 5);
+
+    // for (int j=0; j<7; j++) { k.gate("x", j); }
+
+    prog.add(k);
+
+    prog.compile( );
+}
+
+// steane qec on s7 with cnots
+void
+test_steaneqec(std::string v)
+{
+    int n = 7;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
     float sweep_points[] = { 1 };
 
     ql::quantum_platform starmon("starmon", "test_ideal.json");
@@ -40,8 +166,6 @@ test_steaneqec(std::string v, std::string schedopt, std::string sched_post179opt
 
     prog.add(k);
     
-    ql::options::set("scheduler", schedopt);
-    ql::options::set("scheduler_post179", sched_post179opt);
     prog.compile( );
 }
 
@@ -49,11 +173,11 @@ test_steaneqec(std::string v, std::string schedopt, std::string sched_post179opt
 // no or hardly any significant difference between pre179 and post179 scheduling,
 // slight differences may occur when the json file maps cnot to its constituent primitive gates
 void
-test_manyNN(std::string v, std::string schedopt, std::string sched_post179opt)
+test_manyNN(std::string v)
 {
     int n = 7;
-    std::string prog_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    std::string kernel_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
     float sweep_points[] = { 1 };
 
     ql::quantum_platform starmon("starmon", "test_ideal.json");
@@ -86,140 +210,6 @@ test_manyNN(std::string v, std::string schedopt, std::string sched_post179opt)
 
     prog.add(k);
 
-    ql::options::set("scheduler", schedopt);
-    ql::options::set("scheduler_post179", sched_post179opt);
-    prog.compile( );
-}
-
-// test cnot control operand commutativity
-// i.e. best result is the reverse original order
-void
-test_cnot_controlcommute(std::string v, std::string schedopt, std::string sched_post179opt)
-{
-    int n = 7;
-    std::string prog_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    std::string kernel_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    float sweep_points[] = { 1 };
-
-    ql::quantum_platform starmon("starmon", "test_ideal.json");
-    ql::set_platform(starmon);
-    ql::quantum_program prog(prog_name, starmon, n, 0);
-    ql::quantum_kernel k(kernel_name, starmon, n, 0);
-    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    k.gate("cnot", 3,0);
-    k.gate("cnot", 3,6);
-    k.gate("t", 6);
-    k.gate("y", 6);
-    k.gate("cnot", 3,1);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("cnot", 3,5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    prog.add(k);
-
-    ql::options::set("scheduler", schedopt);
-    ql::options::set("scheduler_post179", sched_post179opt);
-    prog.compile( );
-}
-
-// test cnot target operand commutativity
-// i.e. best result is the reverse original order
-void
-test_cnot_targetcommute(std::string v, std::string schedopt, std::string sched_post179opt)
-{
-    int n = 7;
-    std::string prog_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    std::string kernel_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    float sweep_points[] = { 1 };
-
-    ql::quantum_platform starmon("starmon", "test_ideal.json");
-    ql::set_platform(starmon);
-    ql::quantum_program prog(prog_name, starmon, n, 0);
-    ql::quantum_kernel k(kernel_name, starmon, n, 0);
-    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    k.gate("cnot", 0,3);
-    k.gate("cnot", 6,3);
-    k.gate("t", 6);
-    k.gate("y", 6);
-    k.gate("cnot", 1,3);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("cnot", 5,3);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    prog.add(k);
-
-    ql::options::set("scheduler", schedopt);
-    ql::options::set("scheduler_post179", sched_post179opt);
-    prog.compile( );
-}
-
-// test cz any operand commutativity
-// i.e. best result is the reverse original order
-void
-test_cz_anycommute(std::string v, std::string schedopt, std::string sched_post179opt)
-{
-    int n = 7;
-    std::string prog_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    std::string kernel_name = "test_" + v + "_schedopt=" + schedopt + "_sched_post179opt=" + sched_post179opt;
-    float sweep_points[] = { 1 };
-
-    ql::quantum_platform starmon("starmon", "test_ideal.json");
-    ql::set_platform(starmon);
-    ql::quantum_program prog(prog_name, starmon, n, 0);
-    ql::quantum_kernel k(kernel_name, starmon, n, 0);
-    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    k.gate("cz", 0,3);
-    k.gate("cz", 3,6);
-    k.gate("t", 6);
-    k.gate("y", 6);
-    k.gate("cz", 1,3);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("t", 1);
-    k.gate("y", 1);
-    k.gate("cz", 3,5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-    k.gate("t", 5);
-    k.gate("y", 5);
-
-    // for (int j=0; j<7; j++) { k.gate("x", j); }
-
-    prog.add(k);
-
-    ql::options::set("scheduler", schedopt);
-    ql::options::set("scheduler_post179", sched_post179opt);
     prog.compile( );
 }
 
@@ -227,31 +217,14 @@ int main(int argc, char ** argv)
 {
     ql::utils::logger::set_log_level("LOG_DEBUG");
     ql::options::set("scheduler_uniform", "no");
+    ql::options::set("scheduler", "ALAP");
+    ql::options::set("scheduler_post179", "yes");
 
-//  test_cnot_controlcommute("cnot_controlcommute", "ASAP", "no");
-//  test_cnot_controlcommute("cnot_controlcommute", "ASAP", "yes");
-//  test_cnot_controlcommute("cnot_controlcommute", "ALAP", "no");
-//  test_cnot_controlcommute("cnot_controlcommute", "ALAP", "yes");
-//  test_cnot_targetcommute("cnot_targetcommute", "ASAP", "no");
-//  test_cnot_targetcommute("cnot_targetcommute", "ASAP", "yes");
-//  test_cnot_targetcommute("cnot_targetcommute", "ALAP", "no");
-//  test_cnot_targetcommute("cnot_targetcommute", "ALAP", "yes");
-//  test_cz_anycommute("cz_anycommute", "ASAP", "no");
-//  test_cz_anycommute("cz_anycommute", "ASAP", "yes");
-//  test_cz_anycommute("cz_anycommute", "ALAP", "no");
-//  test_cz_anycommute("cz_anycommute", "ALAP", "yes");
-//  test_steaneqec("steaneqec", "ASAP", "no");
-//  test_steaneqec("steaneqec", "ASAP", "yes");
-//  test_steaneqec("steaneqec", "ALAP", "no");
-    test_steaneqec("steaneqec", "ALAP", "yes");
-//  test_manyNN("manyNN", "ASAP", "no");
-//  test_manyNN("manyNN", "ASAP", "yes");
-//  test_manyNN("manyNN", "ALAP", "no");
-//  test_manyNN("manyNN", "ALAP", "yes");
-
-//  ql::options::set("scheduler_uniform", "yes");
-//  test_hilo("hilo_uniform", "ALAP", "no");
-//  test_hilo("hilo_uniform", "ALAP", "yes");
+    test_cnot_controlcommute("cnot_controlcommute");
+    test_cnot_targetcommute("cnot_targetcommute");
+    test_cz_anycommute("cz_anycommute");
+    test_steaneqec("steaneqec");
+    test_manyNN("manyNN");
 
     return 0;
 }
