9cc351a131724760b29ffcf5ac44ea075e0f60d1
Hans van Someren <j.vansomeren-1@tudelft.nl>
2019-02-11 15:59:34 +0100

    Generation of circuits based on commutable gates now for s17
    Added json file for s17 and imported two tests from Lingling for it.
    Please note that test_ideal.cc thus uses two json files, one for s7 tests and one for s17 tests.

--- tests/test_ideal.cc
+++ tests/test_ideal.cc
@@ -213,6 +213,72 @@ test_manyNN(std::string v)
     prog.compile( );
 }
 
+// steane qec on s17 with cnots
+void
+test_steane17qec1(std::string v)
+{
+    int n = 17;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
+    float sweep_points[] = { 1 };
+
+    ql::quantum_platform starmon("starmon", "test_ideal_17.json");
+    ql::set_platform(starmon);
+    ql::quantum_program prog(prog_name, starmon, n, 0);
+    ql::quantum_kernel k(kernel_name, starmon, n, 0);
+    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
+
+	k.gate("prepz", 5);
+	k.gate("prepz", 2);
+	k.gate("h", 2);
+	k.gate("cnot", 2, 5 );
+	k.gate("cnot", 1, 5);
+	k.gate("cnot", 7, 5);
+	k.gate("cnot", 8, 5);
+	k.gate("cnot", 0, 2 );
+	k.gate("cnot", 2, 5);
+	k.gate("h", 2);
+	k.gate("measure", 5);
+	k.gate("measure", 2);
+
+    prog.add(k);
+    
+    prog.compile( );
+}
+
+// steane qec on s17 with cnots
+void
+test_steane17qec2(std::string v)
+{
+    int n = 17;
+    std::string prog_name = "test_" + v;
+    std::string kernel_name = "test_" + v;
+    float sweep_points[] = { 1 };
+
+    ql::quantum_platform starmon("starmon", "test_ideal_17.json");
+    ql::set_platform(starmon);
+    ql::quantum_program prog(prog_name, starmon, n, 0);
+    ql::quantum_kernel k(kernel_name, starmon, n, 0);
+    prog.set_sweep_points(sweep_points, sizeof(sweep_points)/sizeof(float));
+
+	k.gate("prepz", 5);
+	k.gate("prepz", 2);
+	k.gate("h", 2);
+	k.gate("cnot", 2, 5);
+	k.gate("cz", 1, 5);
+	k.gate("cz", 7, 5);
+	k.gate("cz", 6, 2);
+	k.gate("cz", 0, 2);
+	k.gate("cnot", 2, 5);
+	k.gate("h", 2);
+	k.gate("measure", 5);
+	k.gate("measure", 2);
+
+    prog.add(k);
+    
+    prog.compile( );
+}
+
 int main(int argc, char ** argv)
 {
     ql::utils::logger::set_log_level("LOG_DEBUG");
@@ -223,7 +289,9 @@ int main(int argc, char ** argv)
 //  test_cnot_controlcommute("cnot_controlcommute");
 //  test_cnot_targetcommute("cnot_targetcommute");
 //  test_cz_anycommute("cz_anycommute");
-    test_steaneqec("steaneqec");
+//  test_steaneqec("steaneqec");
+    test_steane17qec1("steane17qec1");
+    test_steane17qec2("steane17qec2");
 //  test_manyNN("manyNN");
 
     return 0;
